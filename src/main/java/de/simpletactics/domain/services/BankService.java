e205ce96d0c744709ea16621b345b673d164b90a
mohjohfox <marcofuchs@mohjohfox.de>
2023-06-01 19:58:28 +0200

    Added zero to points list

--- src/main/java/de/simpletactics/domain/services/BankService.java
+++ src/main/java/de/simpletactics/domain/services/BankService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class BankService {
 
-  private final List<Integer> points = List.of(1, 2, 4, 6, 9, 12, 15, 18, 21, 25);
+  private final List<Integer> points = List.of(0, 1, 2, 4, 6, 9, 12, 15, 18, 21, 25);
   private int pointCounter = 0;
   private int currentCounter = 0;
   private int savedBankCounter = 0;
