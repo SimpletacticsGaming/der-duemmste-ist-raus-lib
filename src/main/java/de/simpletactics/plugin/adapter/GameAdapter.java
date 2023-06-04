23aba8e9bc6eb7312af141718add1a571423c87e
mohjohfox <marcorobinf@gmail.com>
2023-06-04 09:38:39 +0200

    added date to sql statement

--- src/main/java/de/simpletactics/plugin/adapter/GameAdapter.java
+++ src/main/java/de/simpletactics/plugin/adapter/GameAdapter.java
@@ -3,8 +3,10 @@ package de.simpletactics.plugin.adapter;
 import de.simpletactics.domain.models.game.Game;
 import de.simpletactics.domain.services.port.GamePort;
 import de.simpletactics.plugin.database.DataBaseCon;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -26,6 +28,6 @@ public class GameAdapter implements GamePort {
       jdbcTemplate.execute(
           "INSERT INTO quiz_game_finished (id,winner,bank,turns,date) VALUES ('" + game.getGameId() + "', '"
               + game.getWinner().getName()
-              + "', '" + game.getBankAmount() + "', '" + game.getTurns() + "')");
+              + "', '" + game.getBankAmount() + "', '" + game.getTurns() + "', "+ Date.valueOf(LocalDate.now()) +")");
   }
 }
